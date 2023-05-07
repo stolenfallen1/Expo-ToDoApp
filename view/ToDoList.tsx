ea269ed983fd32c054b8aa9343baf393a08d78ba
Jhon Llyod Quizeo <stolenfallen1@gmail.com>
2023-05-07 20:43:19 +0800

    ToDoList styling

--- view/ToDoList.tsx
+++ view/ToDoList.tsx
@@ -1,6 +1,7 @@
 import { Text, View, ScrollView } from "react-native";
 import { useState } from "react";
 import InputForm from "../components/InputForm";
+import { Entypo } from "@expo/vector-icons";
 
 const ToDoList = () => {
   const [items, setItems] = useState<string[]>([]);
@@ -12,9 +13,25 @@ const ToDoList = () => {
   return (
     <View>
       <InputForm onAdd={handleAddItem} />
-      <ScrollView>
+      <ScrollView className="pt-5">
         {items.map((item, index) => (
-          <Text key={index}>{item}</Text>
+          <View key={index} className="flex flex-row items-center">
+            <Text className="text-xl py-3 tracking-tighter w-4/5">{item}</Text>
+            <View className="flex flex-row w-1/5 pl-5">
+              <Entypo
+                name="pencil"
+                size={24}
+                color="blue"
+                className="flex-shrink-0"
+              />
+              <Entypo
+                name="trash"
+                size={24}
+                color="red"
+                className="flex-shrink-0"
+              />
+            </View>
+          </View>
         ))}
       </ScrollView>
     </View>
